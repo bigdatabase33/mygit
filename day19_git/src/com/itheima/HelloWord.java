3a768c9d6b57ea0c43d3e6e95af7bf384faab977
zhangsan <zhangsan@itcast.cn>
2019-09-20 21:41:55 +0800

    第二次提交HelloWord

--- day19_git/src/com/itheima/HelloWord.java
+++ day19_git/src/com/itheima/HelloWord.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("你好啊！git~~~");
+        System.out.println("HelloWord~~~git~~~");
     }
 }
